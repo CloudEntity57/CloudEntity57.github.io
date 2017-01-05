45ec705e3c0cb987cf3dab946b4845683edbfcd7
Josh Foster <josh@allenb.com>
2017-01-05 15:10:01 -0600

    added heliowire not a subrepo

---
+++ heliowire/inc/footer.php
@@ -0,0 +1,14 @@
+</body>
+<footer class="main-footer xs-site-footer inverse panel-footer">
+    
+        <div class="navbar-footer">
+            <?php include("inc/navbar.php"); ?>
+        </div>
+</footer>
+    
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/heliowire.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</html>
---
+++ heliowire/inc/navbar.php
@@ -0,0 +1,23 @@
+
+            <div class="collapse navbar-collapse">
+
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li class="<?php echo $home_on; ?> drop_item">
+                            <?php echo $home; ?>Home</a>
+                        </li>
+                        <li class="<?php echo $blog_on; ?> drop_item">
+                            <?php echo $blog; ?>Blog</a>
+                        </li>
+                        <li class="<?php echo $solarnews_on; ?> drop_item">
+                            <?php echo $solar_news; ?>Solar News</a>
+                        </li>
+                        <li class="drop_item">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="drop_item">
+                            <a href="#">Contact</a>
+                        </li>
+
+                    </ul>
+            </div>
