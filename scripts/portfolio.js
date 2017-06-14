c11a852bd693112a90cb6c56380a1e691d0ecfc8
Josh Foster <josh@allenb.com>
2017-06-14 17:56:41 -0500

    added beta version of couchpolitics

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -22,6 +22,12 @@ $(".navbar-left img").css("width",(.90 * $height));
 //
 $links = [
     {
+        photo: "http://res.cloudinary.com/middle-renaissance-realty-llc/image/upload/c_scale,w_563/v1497474684/CouchPolitics_rt2ufw.png",
+        url:"https://couchpolitics.herokuapp.com/",
+        title: "CouchPolitics Social Network",
+        text:"Full Stack app using React/Redux + Node/Express. Imitates core Facebook functionality. User posts, comments, likes, pages, friend-making. Also filter API news feeds by ideology. With link sharing, privacy settings, user ranking and much more."
+    },
+    {
         photo: "http://res.cloudinary.com/middle-renaissance-realty-llc/image/upload/c_scale,w_564/v1489470677/Untitled_picture_c8abxv.jpg",
         url:"https://smartset-7a283.firebaseapp.com/",
         title: "SmartSet Musician Gig Generator",
